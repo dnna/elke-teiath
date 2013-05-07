428fdd69741af81597c773eece1149258bda7498
Dimosthenis Nikoudis <dnna@dnna.gr>
2013-05-08 01:09:12 +0300

    Timesheet dates are now stored as datetime

--- application/modules/timesheets/forms/Activity.php
+++ application/modules/timesheets/forms/Activity.php
@@ -4,14 +4,16 @@ class  Timesheets_Form_Activity extends Dnna_Form_SubFormBase {
         $this->addElement('text', 'start', array(
             'required' => true,
             'validators' => array(
-            array('validator' => 'Digits'),
+            //array('validator' => 'Digits'),
+            new Zend_Validate_Date(array("format" => 'G:i')),
             new Zend_Validate_Between(array('min' => 1, 'max' => 24, 'inclusive' => true)),
             ),
         ));
         $this->addElement('text', 'end', array(
             'required' => true,
             'validators' => array(
-            array('validator' => 'Digits'),
+            //array('validator' => 'Digits'),
+            new Zend_Validate_Date(array("format" => 'G:i')),
             new Zend_Validate_Between(array('min' => 1, 'max' => 24, 'inclusive' => true)),
             ),
         ));
