1f17d0eaeec99a97f86e997a30b9ca9a57e801fc
Dimosthenis Nikoudis <dnna@dnna.gr>
2013-05-10 23:08:07 +0300

    Bug fix: Minutes in timesheets

--- application/modules/timesheets/forms/Activity.php
+++ application/modules/timesheets/forms/Activity.php
@@ -6,7 +6,7 @@ class  Timesheets_Form_Activity extends Dnna_Form_SubFormBase {
                     if(!is_numeric($parts[0]) || ((int)$parts[0]) < 0 || ((int)$parts[0]) >= 24) {
                         return false;
                     }
-                    if(isset($parts[1]) && (!is_numeric($parts[1]) || ((int)$parts[1]) < 0 || ((int)$parts[1]) > 60)) {
+                    if(isset($parts[1]) && (!is_numeric($parts[1]) || ((int)$parts[1]) < 0 || ((int)$parts[1]) >= 60)) {
                         return false;
                     }
                     return true;
@@ -15,7 +15,7 @@ class  Timesheets_Form_Activity extends Dnna_Form_SubFormBase {
         $this->addElement('text', 'start', array(
             'required' => true,
             'validators' => array(
-			new Zend_Validate_Callback($validate),
+            new Zend_Validate_Callback($validate),
             //array('validator' => 'Digits'),
             //new Zend_Validate_Date(array("format" => 'G')),
             //new Zend_Validate_Between(array('min' => 1, 'max' => 24, 'inclusive' => true)),
@@ -24,7 +24,7 @@ class  Timesheets_Form_Activity extends Dnna_Form_SubFormBase {
         $this->addElement('text', 'end', array(
             'required' => true,
             'validators' => array(
-			new Zend_Validate_Callback($validate),
+            new Zend_Validate_Callback($validate),
             //array('validator' => 'Digits'),
             //new Zend_Validate_Date(array("format" => 'G')),
             //new Zend_Validate_Between(array('min' => 1, 'max' => 24, 'inclusive' => true)),
