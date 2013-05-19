99c2f30be65535073748838f0cce81f90e2e4fb5
Dimosthenis Nikoudis <dnna@dnna.gr>
2013-05-20 01:47:31 +0300

    Educational project timesheets can always be removed

--- application/modules/timesheets/views/helpers/UserCanDelete.php
+++ application/modules/timesheets/views/helpers/UserCanDelete.php
@@ -10,6 +10,11 @@ class Application_View_Helper_UserCanDelete extends Zend_View_Helper_Abstract
     }
 
     public function userCanDelete(Timesheets_Model_Timesheet $timesheet, $enableallagi = false) {
+        // Τα φύλλα στο εκπαιδευτικό έργο επιτρέπεται πάντα να διαγραφούν
+        $options = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getOptions();
+        if($timesheet->get_project() != null && $timesheet->get_project()->get_projectid() == $options['project']['educational']) {
+                return true;
+        }
         if($timesheet->get_approved() == Timesheets_Model_Timesheet::PENDING) {
             return true;
         } else if($timesheet->get_approved() == Timesheets_Model_Timesheet::REJECTED) {
